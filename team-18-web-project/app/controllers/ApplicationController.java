47a8eb0c5994ba7ae1f7458032c121333d8ac48f
Nathan Cheek <nathan@nathancheek.com>
2016-06-07 15:16:08 -0400

    Rename controller etc

--- team-18-web-project/app/controllers/ApplicationController.java
+++ team-18-web-project/app/controllers/ApplicationController.java
@@ -8,7 +8,7 @@ import views.html.*;
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class HomeController extends Controller {
+public class ApplicationController extends Controller {
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -16,9 +16,7 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(index.render("Welcome"));
-    }
+    public Result home() { return ok(home.render("Welcome")); }
 
     public Result about() { return ok(about.render()); }
 
