73b8f26d0d92daf9f090eb5b0f955d461614bfe9
Nathan Cheek <nathan@nathancheek.com>
2016-06-13 19:39:44 -0400

    Remove cruft

--- team-18-web-project/app/models/User.java
+++ team-18-web-project/app/models/User.java
@@ -22,18 +22,12 @@ public class User extends Model {
     public int account_locked;
     public int login_attempts;
 
-    /*public User(String email, String name, String password, String username) {
+    /*public User(String email, String name, String password, String username) { //This is probably wrong
         this.email = email;
         this.name = name;
         this.password = password;
         this.username = username;
     }*/
 
-    /*public static Finder<String,User> find = new Finder<String,User>(
-            String.class, User.class
-    );*/
-
     public static Finder<String, User> find = new Finder<String,User>(User.class);
-
-    //public static final Find<String, User> find = new Find<String,User>(){};
 }
